2737c711f5a8db4406c3e40f41f4b39a40966e84
Jack <jackstasiulis@gmail.com>
2022-12-09 14:47:57 -0800

    add basic code to sign in and register pages

---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,16 @@
+import './Register.scss'
+import LogoRcrop from '../../assets/Backyard-logo-black.png';
+
+function Register () {
+    return(
+        <div className='register'>
+            <form className='register__form' action=""></form>
+                
+        </div>
+    )
+    
+}
+
+export default Register;
+
+
