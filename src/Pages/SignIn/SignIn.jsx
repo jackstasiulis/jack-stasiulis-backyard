fec3485ea915ce06688b33bee1f0ad2c027d4a42
Jack <jackstasiulis@gmail.com>
2022-12-09 14:47:14 -0800

    add register page

---
+++ src/Pages/SignIn/SignIn.jsx
@@ -0,0 +1,16 @@
+import './SignIn.scss'
+import LogoRcrop from '../../assets/Backyard-logo-black.png';
+
+function SignIn () {
+    return(
+        <div className='signin'>
+            <form className='signin__form' action=""></form>
+                
+        </div>
+    )
+    
+}
+
+export default SignIn;
+
+
